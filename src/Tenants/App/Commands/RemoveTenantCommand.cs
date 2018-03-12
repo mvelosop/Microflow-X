4687175c3fa61e6f4325f3ae2c61734fd7555cda
Miguel Veloso <mvelosop@gmail.com>
2018-03-12 12:39:49 +0000

    Done with RemoveTenantCommand

---
+++ src/Tenants/App/Commands/RemoveTenantCommand.cs
@@ -0,0 +1,22 @@
+﻿using Domion.Base;
+using MediatR;
+using System;
+using Tenants.Core.Model;
+
+namespace Tenants.App.Commands
+{
+    public class RemoveTenantCommand : IRequest<CommandResult>
+    {
+        public RemoveTenantCommand(Guid id, byte[] updateToken)
+        {
+            Id = id;
+            UpdateToken = updateToken ?? throw new ArgumentNullException(nameof(updateToken));
+
+            if (updateToken.Length == 0) throw new ArgumentException("Value cannot be an empty collection.", nameof(updateToken));
+        }
+
+        public Guid Id { get; }
+
+        public byte[] UpdateToken { get; }
+    }
+}
